9dc07ea87b349ae7d41860535c47eab185f24229
uiwwnw Yoon <uiwwnwyoon@uiwwnwui-MacBook.local>
2018-03-31 23:59:22 +0900

    react 기본세팅 제거 basic

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
-const webpack = require('webpack');
 const path = require('path');
-const SpritesmithPlugin = require('webpack-spritesmith');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
@@ -8,92 +6,24 @@ const extractSass = new ExtractTextPlugin({
     disable: process.env.NODE_ENV === 'development'
 });
 
-var config = {
-    context: path.resolve(__dirname, 'src'),
-    entry: './index.js',
-    output: {
-        path: __dirname,
-        filename: './dist/bundle.js'
-    },
-
-    devServer: {
-        inline: true,
-        port: 7777,
-        contentBase: __dirname + 'dist/'
-    },
-    devtool: 'source-map', // any 'source-map'-like devtool is possible
-    module: {
-        loaders: [
-            {
-                test: /\.scss$/,
-                use: [{
-                    loader: 'style-loader'
-                }, {
-                    loader: 'css-loader', options: {
-                        sourceMap: true
-                    }
-                }, {
-                    loader: 'sass-loader', options: {
-                        sourceMap: true
-                    }
-                }]
-            }
-            , {
-                test: /\.png$/,
-                loaders: [
-                    'file-loader?name=./dist/img/[hash].[ext]'
-                ]
-            }
-            , {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    cacheDirectory: true,
-                    presets: ['es2015', 'react']
-                }
-            }
-            , {
-                test: /\.(woff|woff2|ttf|eot|svg)(\?v=[a-z0-9]\.[a-z0-9]\.[a-z0-9])?$/,
-                loader: 'url-loader?name=./dist/img/[hash].[ext]'
-            }
-        ]
-    },
-    resolve: {
-        //webpack 1:
-        // modulesDirectories: ['node_modules', 'spritesmith-generated'],
-        //webpack 2:
-        modules: ['node_modules', 'spritesmith-generated']
-    },
-    plugins: [
-        new SpritesmithPlugin({
-            src: {
-                cwd: path.resolve(__dirname, 'src/ico'),
-                glob: '*.png'
-            },
-            target: {
-                image: path.resolve(__dirname, 'src/spritesmith-generated/sprite.png'),
-                css: path.resolve(__dirname, 'src/spritesmith-generated/sprite.scss')
-            },
-            apiOptions: {
-                cssImageRef: './spritesmith-generated/sprite.png'
-            }
-        })
-        , extractSass
-    ]
-
-};
-
-if (process.env.NODE_ENV === 'production') {
-    config.devtool = false;
-    config.plugins = [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({ comments: false }),
-        new webpack.DefinePlugin({
-            'process.env': { NODE_ENV: JSON.stringify('production') }
-        })
-    ];
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './bundle.js'
+  },
+  mode: 'production',
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [{
+            loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+            loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+            loader: 'sass-loader' // compiles Sass to CSS
+        }]
+    }]
 }
-
-
-module.exports = config;
+};
