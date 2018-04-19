c0c977e52fa39ae8f5a8e99c28011d5456754da2
uiwwnw Yoon <uiwwnw@icloud.com>
2018-04-19 16:23:33 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,49 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
-    filename: '[name].[contenthash].css',
-    disable: process.env.NODE_ENV === 'development'
+  filename: '[name].[contenthash].css',
+  disable: process.env.NODE_ENV === 'development'
 });
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  entry: './app.js',
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: './bundle.js'
   },
+  devServer: {
+    inline: true,
+    port: 3000,
+    hot: true
+  },
   mode: 'production',
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.scss$/,
-        use: [{
+        use: [
+          {
             loader: 'style-loader' // creates style nodes from JS strings
-        }, {
+          },
+          {
             loader: 'css-loader' // translates CSS into CommonJS
-        }, {
+          },
+          {
             loader: 'sass-loader' // compiles Sass to CSS
-        }]
-    }]
-}
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  performance: { hints: false }
 };
