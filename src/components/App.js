f4b1799409ce17ed563a6289c60aa094f04fde5d
uiwwnw Yoon <uiwwnwyoon@uiwwnwui-MacBook.local>
2018-10-18 23:44:01 +0900

    version bump from `1.0.0` to `1.0.1` - 스타일드컴포넌츠 추가 및 스타일파일 삭제

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import Style from '../style.scss';
+import styled from 'styled-components';
+const Contents = styled.div`
+    color: red;
+    font-size: 20px;
+`;
 
 export default class App extends React.Component {
     constructor() {
@@ -8,10 +12,11 @@ export default class App extends React.Component {
     }
     render() {
         return (
-            <main className={Style.main}>
+            <main>
                 <div>
                     안녕하세요 반가워요 프론트엔드개발자 리엑트 패키지입니다.
                     편하게사용해주세요~
+                    <Contents>ddd</Contents>
                 </div>
             </main>
         );
